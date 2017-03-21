5a229be2af5cd1a83d6287f4b6d3e5db8a5f37b3
Amogh100 <2amogh@gmail.com>
2017-03-21 10:08:13 -0600

    Updated Database_Code and created Population.sql

--- Database_Code.sql
+++ Database_Code.sql
@@ -1,5 +1,9 @@
+/*
+Creating database and tables
+*/
+
 Create database Coursemate;
-CREATE TABLE User (First_Name VARCHAR(35), Last_Name VARCHAR(35), Username VARCHAR(16), Password INTEGER, Email VARCHAR(254), Photo_Location VARCHAR(100), User_ID INTEGER, PRIMARY KEY(Username, Password));
+CREATE TABLE User (First_Name VARCHAR(35), Last_Name VARCHAR(35), Username VARCHAR(16), Password VARCHAR(50), Email VARCHAR(254), Photo_Location VARCHAR(100), User_ID INTEGER, PRIMARY KEY(Username, Password));
 CREATE TABLE File(Location VARCHAR(100), File_Name VARCHAR(150), User_ID INTEGER, Uploaded DATE, File_ID INTEGER, PRIMARY KEY(FILE_ID));
 CREATE TABLE Class(Class_Name VARCHAR(150), Class_ID INTEGER, Instructor_First VARCHAR(35), Instructor_Last VARCHAR(35), CA_First VARCHAR(35), CA_Last VARCHAR(35), PRIMARY KEY(Class_ID, CA_First, CA_Last));
 CREATE TABLE Post(Post_Name VARCHAR(150), File_ID INTEGER, User_ID INTEGER, Class_ID INTEGER, Time DATE, Post_Text VARCHAR(MAX), PRIMARY KEY(Post_name, File_ID, Class_ID, Time, User_ID));
---
+++ Populate.sql
@@ -0,0 +1,54 @@
+INSERT INTO User(First_Name, Last_Name, Username, Password, Email, Photo_Location, User_ID)
+            VALUES("Amogh", "Jahagirdar", "AmoghUsername", "BcryptedAmoghPassword", "Amogh.Jahagirdar@Colorado.edu", "HashedAmoghPhotoLocationAWS", 1);
+
+INSERT INTO User(First_Name, Last_Name, Username, Password, Email, Photo_Location, User_ID)
+            VALUES("Joshua", "Ramos", "JoshuaUsername", "BcryptedJoshuaPassword", "Joshua.Ramos@Colorado.edu", "HashedJoshuaPhotoLocationAWS", 2);
+
+INSERT INTO User(First_Name, Last_Name, Username, Password, Email, Photo_Location, User_ID)
+            VALUES("Elliott", "Shugerman", "ElliottUsername", "BcryptedElliottPassword", "Elliott.Shugerman@Colorado.edu", "HashedElliottPhotoLocationAWS", 3);
+
+INSERT INTO User(First_Name, Last_Name, Username, Password, Email, Photo_Location, User_ID)
+            VALUES("Alexander", "Fisher", "AlexanderUsername", "BcryptedAlexanderPassword", "Alexander.M.Fisher@Colorado.edu", "HashedAlexanderPhotoLocationAWS", 4);
+
+INSERT INTO User(First_Name, Last_Name, Username, Password, Email, Photo_Location, User_ID)
+            VALUES("Ruben", "Vargas", "RubenUsername", "BcryptedRubenPassword", "Ruben.Vargas@Colorado.edu", "HashedRubenPhotoLocationAWS", 5);
+
+
+INSERT INTO File(Location, File_Name, User_ID, Uploaded, File_ID)
+            VALUES("practiceExam1location", "practiceExam1.pdf", 2, "2017-02-05", 1);
+
+INSERT INTO File(Location, File_Name, User_ID, Uploaded, File_ID)
+            VALUES("practiceExam2location", "practiceExam2.pdf", 4, "2017-02-07", 2);
+
+INSERT INTO File(Location, File_Name, User_ID, Uploaded, File_ID)
+            VALUES("practiceExam3location", "practiceExam3.pdf", 5, "2017-02-09", 3);
+
+INSERT INTO File(Location, File_Name, User_ID, Uploaded, File_ID)
+            VALUES("AlgorithmsNotes1Location", "AlgoNotes.ppt", 1, "2017-02-08", 4);
+
+INSERT INTO File(Location, File_Name, User_ID, Uploaded, File_ID)
+            VALUES("SoftwareDevNotes1Location", "SoftwareNotes.txt", 3, "2017-03-10", 5);
+
+INSERT INTO Class(Class_Name, Class_ID, Instructor_First, Instructor_Last, CA_First, CA_Last)
+            VALUES("Software Development and Tools", 3308, "Alan", "Paradise", "Yogitha", "Mahadasu");
+
+INSERT INTO Class(Class_Name, Class_ID, Instructor_First, Instructor_Last, CA_First, CA_Last)
+            VALUES("Statistical Methods", 4570, "Yolanda", "Slichter", "Amogh", "Jahagirdar");
+
+INSERT INTO Class(Class_Name, Class_ID, Instructor_First, Instructor_Last, CA_First, CA_Last)
+            VALUES("Algorithms", 3104, "Aaron", "Clauset", "Cormen", "Leirson");
+
+INSERT INTO Class(Class_Name, Class_ID, Instructor_First, Instructor_Last, CA_First, CA_Last)
+            VALUES("Discrete Structures", 2824, "Mike", "Eisenberg", "George", "Polya");
+
+INSERT INTO Post(Post_Name, File_ID, User_ID, Class_ID, Time, Post_Text)
+            VALUES("Practice Exam 1 for Discrete Structures",1,1,2824,"2017-02-06", "Attached is the 1st Practice Exam for Discrete Structures");
+
+INSERT INTO Post(Post_Name, File_ID, User_ID, Class_ID, Time, Post_Text)
+            VALUES("Practice Exam 2 for Algorithms",2,3,3104, "2017-02-08", "Attached is the 2nd Practice Exam for Algorthms");
+
+INSERT INTO Post(Post_Name, File_ID, User_ID, Class_ID, Time, Post_Text)
+            VALUES("Practice Exam 3 for Statistical Methods",3,4,4570, "2017-02-10", "Attached is the 3rd Practice Exam for Statistical Methods");
+
+INSERT INTO Post(Post_Name, File_ID, User_ID, Class_ID, Time, Post_Text)
+            VALUES("Updated Software Development Notes",5,5,3308, "2017-03-11", "Attached is the updated Software Dev notes");
