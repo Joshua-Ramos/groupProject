fd373edf61da99e28fbd9709687babfc516fbff3
Amogh Jahagirdar <amoghjahagirdar@Amoghs-MacBook-Pro.local>
2017-04-30 10:54:38 -0600

    Added back s3_download, bugfix with email during logon, and other small tweaks

--- coursemate/config.py
+++ coursemate/config.py
@@ -2,12 +2,11 @@ class DevConfig:
     DEVELOPTMENT = True
 
     # AWS S3
-    S3_KEY = 'AKIAIQ47KM3UPFPDXR7A'
-    S3_SECRET = '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'
+    S3_KEY = ''
+    S3_SECRET = ''
     S3_BUCKET = 'coursemat' # typo?
     S3_UPLOAD_DIRECTORY = 'Testing'
     SECRET_KEY = 'TestingSecretKey'
 
     # SQL
-    # SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/CourseMate'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Jahag@123@localhost/CourseMate'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:password@localhost/CourseMate'
